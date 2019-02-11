8cbc7ec61a413c0d96139465975bcea6b7de83e0
Rodrigo Almeida <rodrigoalmeida.contato@gmail.com>
2019-02-11 20:51:26 -0200

    users api returning from firebase admin

--- components/users/users-api.router.js
+++ components/users/users-api.router.js
@@ -1,12 +1,14 @@
+const admin = require('firebase-admin');
+
 module.exports = function(app) {
     app.route('/api/users')
-        .get((req, res) => {
-            res.json({
-                name: {
-                    first: 'Rodrigo',
-                    last: 'Almeida'
-                },
-                msg: 'Simple json to be replaced with a database query'
+        .get((req, res) => {          
+            admin.auth().listUsers(1000)
+            .then(function(listUsersResult) {
+                res.send(listUsersResult.users);
+            })
+            .catch(function(error) {
+                console.log("Error listing users:", error);
             });
         });
 }
